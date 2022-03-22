a25c770466f0ebad7a6dfc863df555efc78970a7
Ferdinand <91031979+cusihuaman@users.noreply.github.com>
2022-03-22 18:20:51 -0500

    Subiendo el proyecto de sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,52 @@
-const {src,dest,watch} = require ('gulp');
+const {src,dest,watch,series,parallel} = require ('gulp');
+//css y sass
 const sass=require('gulp-sass')(require('sass'));
 const postcss=require('gulp-postcss');
 const autoprefixer=require('autoprefixer');
+//imagenes
+const imagemin = require('gulp-imagemin');
+const webp=require('gulp-webp');
+const avif=require('gulp-avif');
 
 function css(done){
     //compilar sass
     src('src/scss/app.scss')//identificar archivo
-        .pipe(sass({outputStyle:'compressed'}) )//compilar
+        .pipe(sass() )//compilar
         .pipe(postcss([autoprefixer()]))
         .pipe(dest('build/css'))//guardar el css
     done();
 }
-function dev(){
-    watch('src/scss/app.scss', css)
+function imagenes(){
+    return src('src/img/**/*')
+    .pipe(imagemin({optimizationLevel:3}))
+    .pipe(dest('build/img'))
 }
+function versionWebp(){
+    const opciones={
+        quality:50
+    }
+    return src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'))
+}
+function versionAvif(){
+    const opciones={
+        quality:50
+    }
+    return src('src/img/**/*.{png,jpg}')
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'))
+}
+function dev(){
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
+}
+
 exports.css =css;
 exports.dev=dev;
+exports.imagenes=imagenes;
+exports.versionWebp=versionWebp;
+exports.versionAvif=versionAvif;
+exports.default=series(imagenes,versionWebp, versionAvif, css,dev);
+//series= se inicia una tarea y hasta que finaliza inicia el siguiente
+//parallel=todas inician al mismo tiempo.
